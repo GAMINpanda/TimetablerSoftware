46d29f91db20910e96fb16c9141fbef0a0d15138
Alexander Mitchiner <mitchinerfamily1@gmail.com>
2022-03-23 16:03:03 +0000

    started functionality to add inputted activities onto the timetable (also added 0700 - 2200 hrs)

--- MainCode.cs
+++ MainCode.cs
@@ -31,6 +31,32 @@ namespace MainCode
             Days = InputDays;
         }
 
+        public int GetRowBegin()
+        {
+            //Row on WPF grid is different to actuality
+            //==> Rows start at 2 inclusive
+
+            int bg = Convert.ToInt32(BeginningTime);
+            bg = bg - 5;
+            bg = bg / 100;
+            return bg;
+        }
+
+        public List<int> GetColumnBegin()
+        {
+            //columns start on 4 inclusive
+            int count = 0;
+            List<int> dayscol = new List<int>();
+            foreach (bool value in Days)
+            {
+                if (value) {
+                    dayscol.Add(count + 4);
+                }
+                count++;
+            }
+            return dayscol;
+        }
+
         public string BeginningTime { get; }
         public string EndingTime { get; }
         public string ActivityName { get; }
