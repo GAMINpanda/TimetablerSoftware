18fac4a6f92479ca1d4c0526e8aacec135f50a21
Alexander Mitchiner <83463486+GAMINpanda@users.noreply.github.com>
2022-09-30 17:35:17 +0100

    Now list<list<string>> created with all possible times, so progam can choose a valid time.

--- GenerateTime.cs
+++ GenerateTime.cs
@@ -10,15 +10,8 @@ namespace GenSlots
 {
     public class NewTimeGen : NewTimeSlot //':' inherits
     {
-        static void GetActvitiesToGenerate()
+        static List<string> GetCSVData(string filename)
         {
-
-        }
-
-        public static void FreeTime() //Reads save file to decide what times are free in the week - Creates new file with details (call once on save)
-        {
-            string[] Day;
-            string filename = "Save.csv";
             string FullPath = Path.GetFullPath(filename);
 
             FullPath = FullPath.Replace(@"bin\Debug\netcoreapp3.1\", "");
@@ -27,9 +20,25 @@ namespace GenSlots
 
             string[] CSVitems = (file.ReadToEnd()).Split('\n'); //seperate activities
 
+            file.Close();
+
             List<string> CorrectCSVitems = new List<string>(CSVitems);
 
-            CorrectCSVitems.RemoveAt(CSVitems.Length - 1); //cast to list then remove final (empty) space
+            return CorrectCSVitems;
+        }
+
+        static void GetActvitiesToGenerate()
+        {
+
+        }
+
+        public static void FreeTime() //Reads save file to decide what times are free in the week - Creates new file with details (call once on save)
+        {
+            string[] Day;
+
+            List<string> CorrectCSVitems = GetCSVData("Save.csv");
+
+            CorrectCSVitems.RemoveAt(CorrectCSVitems.Count - 1); //cast to list then remove final (empty) space
 
             /*
             foreach(string val in CSVitems)
@@ -37,8 +46,6 @@ namespace GenSlots
                 Debug.WriteLine(val);
             }
             */
-
-            file.Close();
 
             //Now adding values To SaveBoundaries for easy retrieval
 
@@ -58,8 +65,8 @@ namespace GenSlots
             }
 
 
-            filename = "SaveBoundaries.csv";
-            FullPath = Path.GetFullPath(filename);
+            string filename = "SaveBoundaries.csv";
+            string FullPath = Path.GetFullPath(filename);
 
             FullPath = FullPath.Replace(@"bin\Debug\netcoreapp3.1\", "");
 
@@ -73,10 +80,67 @@ namespace GenSlots
             filenew.Close();
         }
 
+        public static List<List<string>> ExtractFromSaveBoundaries() //converts into list with all free times
+        {
+            List<string> defaultvar = new List<string>() { "0700", "0800", "0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200"};
+            List<List<string>> allvalues = new List<List<string>>() { defaultvar, defaultvar, defaultvar, defaultvar, defaultvar, defaultvar, defaultvar };
+            List<string> CSVitems = GetCSVData("SaveBoundaries.csv");
+            int len;
+            List<List<string>> Timing = new List<List<string>>();
+
+            foreach(string value in CSVitems)
+            {
+                Timing.Add( new List<string> (value.Split(','))); //list of times taken
+            }
+
+            for(int i = 0; i < 7; i++)
+            {
+                len = Timing[i].Count;
+
+                for (int j = 0; j < len; j = j + 2)
+                {
+                    allvalues[i] = EachDay(Timing[i][j], Timing[i][j+1], allvalues[i]);
+                }
+            }
+
+            return allvalues;
+        }
+
+        public static List<string> EachDay(string time1, string time2, List<string> Day) //removes times from day list in range of time1 --> time2
+        {
+            int curtime;
+            string curtimestring;
+            int firsttime = Convert.ToInt32(time1);
+            int secondtime = Convert.ToInt32(time2);
+
+            int count = firsttime - secondtime;
+            count = count / 100;
+
+            for (int i = 0; i < count; i++)
+            {
+                curtime = firsttime + (i*100); //gaps of 100
+
+                if (curtime < 1000)
+                {
+                    curtimestring = "0" + Convert.ToString(curtime);
+                }
+                else
+                {
+                    curtimestring = Convert.ToString(curtime);
+                }
+
+                Day.Remove(curtimestring);
+            }
+
+            return Day;
+        }
+
         static void DecideActivityDistribution()
         {
 
         }
+
+        private List<List<string>> freetimevals = NewTimeGen.ExtractFromSaveBoundaries();
     }
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # TimetablerSoftware
 Developing a software to automatically generate a timetable for a person's needs.
 
-So far have the ability to save and remove times but doesn't enact on Canvas yet
+Currently figuring out how to let the program figure out times avaliable
